3f788ad274642f922f6a1c8346035356a1007232
Blueguardian <tssa20@student.aau.dk>
2021-04-22 22:03:15 +0200

    comments

--- config.py
+++ config.py
@@ -9,20 +9,35 @@ from configparser import ConfigParser  # Import the configprser for reading the 
 class CaseConfig:
     # Global variables
     # Set allowed values for covers
-    _ALLOWED_COLOURS = ['white', 'blue', 'black']  # Allowed colors white, blue and black
-    _ALLOWED_CURVES = ['none', 'edge', 'curved']  # Allowed curve types none, edge and curved
+    _ALLOWED_COLOURS = ['white', 'blue', 'black']  # List of allowed colors white, blue and black
+    _ALLOWED_CURVES = ['none', 'edge', 'curved']  # List of allowed curve types none, edge and curved
+
+    # Class method definition:
+    # __open_config.
+    # Class methods are static, and has access to class methods
+    # without having to create an instance of an object first.
+    # This method accesses the config file and returns a parser to
+    # the file
+
+    @classmethod  # Decorator
+    def __open_config(cls) -> ConfigParser:  # Method definition with return type hint ConfigParser
+        cfg = ConfigParser()  # Initialize object cfg with ConfigParser object
+        cfg.read('config.ini')  # Read the config file
+        return cfg  # return the cfg object
+
+    # Class method definition
+    # colour.
+    # Class methods are static, and has access to class methods
+    # without having to create an instance of an object first.
+    # This method accesses and reads the "colour" data from customer
+    # checks whether it corresponds to one of the values in _ALLOWED_COLOURS
+    # and returns the value if it is.
 
     @classmethod
-    def __open_config(cls) -> ConfigParser:
-        cfg = ConfigParser()
-        cfg.read('config.ini')
-        return cfg
+    def colour(cls, type_='customer'):  # Method definition with parameter default 'customer'
+        colour_id = cls.__open_config().get(type_, 'colour')  # Open the file and read the value at identifier colour in type_
 
-    @classmethod
-    def colour(cls, _type='costumer'):
-        colour_id = cls.__open_config().get(_type, 'colour')
-
-        # if you decide to use hex strings instead of colour names, use re to ensure it is valid
+        # if we decide to use hex strings instead of colour names, use re to ensure it is valid
         # import re
         #
         # if not re.match('[a-fA-F0-9]', colour_id'):
@@ -31,37 +46,62 @@ class CaseConfig:
         # and append the # when returning
         # return f'#{colour_id}'
 
-        if colour_id not in cls._ALLOWED_COLOURS:
-            raise KeyError('Error, colour is not available.')
+        if colour_id not in cls._ALLOWED_COLOURS:  # If the value is not in the list
+            raise KeyError('Error, colour is not available.')  # Raise the exception KeyError with fitting text
 
         # ... other error handling, validation, or mutation
 
-        return colour_id
+        return colour_id  # Return the value
+
+    # Class method definition
+    # curve_style.
+    # Class methods are static, and has access to class methods
+    # without having to create an instance of an object first.
+    # This method accesses and reads the "curve_style" data from type_
+    # checks whether it corresponds to one of the values in _ALLOWED_CURVES
+    # and returns the value if it is.
 
     @classmethod
-    def curve_style(cls, _type='customer'):
-        curve_style_id = cls.__open_config().get(_type, 'curve_style')
+    def curve_style(cls, type_='customer'):  # Method definition with parameter default 'customer'
+        curve_style_id = cls.__open_config().get(type_, 'curve_style')  # Open the file and read the value at identifier curve_style in type_
 
-        if curve_style_id not in cls._ALLOWED_CURVES:
-            raise KeyError('Error, curve style is not available.')
+        if curve_style_id not in cls._ALLOWED_CURVES:  # If the value is not in the list
+            raise KeyError('Error, curve style is not available.')  # Raise the exception  KeyError with fitting text
 
         # ... other error handling, validation, or mutation
 
-        return curve_style_id
+        return curve_style_id  # Return the value
+
+    # Class method definition
+    # curve_style.
+    # Class methods are static, and has access to class methods
+    # without having to create an instance of an object first.
+    # This method accesses and reads the "file" data from type_
+    # checks whether it is a file and returns the value if it is
 
     @classmethod
-    def file(cls, _type='customer'):
-        file_path = cls.__open_config().get(_type, 'file')
+    def file(cls, type_='customer'):  # Method definition with parameter default 'customer'
+        file_path = cls.__open_config().get(type_, 'file')  # Open the file and read the value at identifier file in type_
 
-        if not os.path.isfile(file_path):
-            raise FileNotFoundError('Error, file does not exist.')
+        if not os.path.isfile(file_path):  # If the value does not correspond to a file
+            raise FileNotFoundError('Error, file does not exist.')  # Raise the exception FileNotFoundError with fitting text
 
         # ... other error handling, validation, or mutation
 
-        return file_path
+        return file_path  # Return the value
 
-    @classmethod
-    def engrave(cls, _type='costumer'):
-        engrave_bool = cls.__open_config().getboolean(_type, 'engrave')
+    # Class method definition
+    # curve_style.
+    # Class methods are static, and has access to class methods
+    # without having to create an instance of an object first.
+    # This method accesses and reads the "engrave" data from type_
+    # checks whether it is a boolean and returns the value if it is.
 
-        return engrave_bool
+    @classmethod # Decorator
+    def engrave(cls, type_='customer'):  # Method definition with parameter default 'customer'
+        engrave_bool = cls.__open_config().getboolean(type_, 'engrave') # Open the file and read the value at identifier colour in type
+
+        if type(engrave_bool) != bool:  # If the value is not a boolean
+            raise KeyError('Error, not a boolean type')  # Raise the exception KeyError with fitting text
+
+        return engrave_bool   # Return the value
--- engrave.py
+++ engrave.py
@@ -8,37 +8,41 @@ import sys
 
 
 class Engrave:
-    _APPROACH = 100
+    _APPROACH = 100  # Approach distance to objects
     IMAGE_SIZE_FLAT = svg.Point(112, 58)  # size of the image in MM not including edge (2mm)
     IMAGE_SIZE_EDGE = svg.Point(104, 40)  # size of image in MM not including edge (??? mm)
     IMAGE_SIZE_CURVED = svg.Point(112, 58)  # size of image in MM not including edge (??? mm)
-    RDK = Robolink()
+    RDK = Robolink()  # Establish contact with RoboDK
+
+    # Constructor definition
+    # Always called __init__
+    # Defined to independently to collect data from the
+    # config file.
 
     def __init__(self):
-        if os.path.isfile(CaseConfig.file()):
-            self.svg = svg.svg_load(CaseConfig.file())
+        if os.path.isfile(CaseConfig.file()):  # If the given path is a file
+            self.svg = svg.svg_load(CaseConfig.file())  # Load that file
         else:
-            self.svg = svg.svg_load(CaseConfig.file('DEFAULT'))
-        self.curve = CaseConfig.curve_style()
-        self.color = CaseConfig.colour()
+            self.svg = svg.svg_load(CaseConfig.file('DEFAULT'))  # Else load a default file
+        self.curve = CaseConfig.curve_style()  # Load the curve from the customer
+        self.color = CaseConfig.colour()  # Load the color from the customer
 
     def point2D_2_pose(self, point, tangent):
-        """Converts a 2D point to a 3D pose in the XY plane including rotation being tangent to the path"""
         return robodk.transl(point.x, point.y, 0) * robodk.rotz(tangent.angle())
 
-    def begin(self):
+    def begin_flat(self):
         robot = self.RDK.Item('cart_robot')
         home_trans = robot.JointsHome()
         item_frame = robot.Childs()
         tool_frame = self.RDK.Item('tool')
         pix_ref = self.RDK.Item('pixel')
         self.move_to_environment()
-        item = self.RDK.Item(f'cover_{self.color}_{self.curve}')
+        item = self.RDK.Item(f'cover_{self.color}_none')
 
         robot.setPoseFrame(robot.Childs())
         robot.setPoseTool('tool')
 
-        if self.curve == 'none' and robot.item.Valid():
+        if robot.item.Valid():
             self.svg.calc_polygon_fit(self.IMAGE_SIZE_FLAT)
             # size_img = self.svg.size_poly() # Uncertain if needed
             for path in self.svg:
@@ -53,8 +57,98 @@ class Engrave:
                 target0 = robodk.transl(p_0.x, p_0.y, 0) * orient_frame2tool
                 target0_app = target0 * robodk.transl(0, 0, -self._APPROACH)
                 robot.MoveL(target0_app)
+
+                self.RDK.RunMessage('Drawing %s' % path.idname)
+                self.RDK.RunProgram('SetColorRGB(%.3f,%.3f,%.3f)' % (path.fill_color[0], path.fill_color[1], path.fill_color[2]))
+                for i in range(np):
+                    p_i = path.getPoint(i)
+                    v_i = path.getVector(i)
+
+                    pt_pose = self.point2D_2_pose(p_i, v_i)
+                    target = robodk.transl(p_i.x, p_i.y, 0) * orient_frame2tool
+                    robot.MoveL(target)
+
+                    # create a new pixel object with the calculated pixel pose
+                    item.Paste().setPose(pt_pose)
+                    target_app = target * robodk.transl(0, 0, -self._APPROACH)
+                    robot.MoveL(target_app)
+
+                robot.MoveL(home_trans)
+
+    def begin_edge(self):
+        robot = self.RDK.Item('cart_robot')
+        home_trans = robot.JointsHome()
+        item_frame = robot.Childs()
+        tool_frame = self.RDK.Item('tool')
+        pix_ref = self.RDK.Item('pixel')
+        self.move_to_environment()
+        item = self.RDK.Item(f'cover_{self.color}_edge')
+
+        robot.setPoseFrame(robot.Childs())
+        robot.setPoseTool('tool')
+
+        if robot.item.Valid():
+            self.svg.calc_polygon_fit(self.IMAGE_SIZE_EDGE)
+            # size_img = self.svg.size_poly() # Uncertain if needed
+            for path in self.svg:
+                # use the pixel reference to set the path color, set pixel width and copy as a reference
+                print('Drawing %s, RGB color = [%.3f,%.3f,%.3f]' % (path.idname, path.fill_color[0], path.fill_color[1], path.fill_color[2]))
+                pix_ref.Recolor(path.fill_color)
+                pix_ref.Copy()
+                np = path.nPoints()
+                p_0 = path.getPoint(0)
+                orient_frame2tool = robodk.invH(item_frame.Pose()) * robot.SolveFK(home_trans) * tool_frame.Pose()
+                orient_frame2tool[0:3, 3] = robodk.Mat([0, 0, 0])
+                target0 = robodk.transl(p_0.x, p_0.y, 0) * orient_frame2tool
                 target0_app = target0 * robodk.transl(0, 0, -self._APPROACH)
                 robot.MoveL(target0_app)
+
+                self.RDK.RunMessage('Drawing %s' % path.idname)
+                self.RDK.RunProgram('SetColorRGB(%.3f,%.3f,%.3f)' % (path.fill_color[0], path.fill_color[1], path.fill_color[2]))
+                for i in range(np):
+                    p_i = path.getPoint(i)
+                    v_i = path.getVector(i)
+
+                    pt_pose = self.point2D_2_pose(p_i, v_i)
+                    target = robodk.transl(p_i.x, p_i.y, 0) * orient_frame2tool
+                    robot.MoveL(target)
+
+                    # create a new pixel object with the calculated pixel pose
+                    item.Paste().setPose(pt_pose)
+                    target_app = target * robodk.transl(0, 0, -self._APPROACH)
+                    robot.MoveL(target_app)
+
+                robot.MoveL(home_trans)
+
+    def begin_curved(self):
+        robot = self.RDK.Item('cart_robot')
+        home_trans = robot.JointsHome()
+        item_frame = robot.Childs()
+        tool_frame = self.RDK.Item('tool')
+        pix_ref = self.RDK.Item('pixel')
+        self.move_to_environment()
+        item = self.RDK.Item(f'cover_{self.color}_curved')
+
+        robot.setPoseFrame(robot.Childs())
+        robot.setPoseTool('tool')
+
+        if robot.item.Valid():
+            self.svg.calc_polygon_fit(self.IMAGE_SIZE_EDGE)
+            # size_img = self.svg.size_poly() # Uncertain if needed
+            for path in self.svg:
+                # use the pixel reference to set the path color, set pixel width and copy as a reference
+                print('Drawing %s, RGB color = [%.3f,%.3f,%.3f]' % (path.idname, path.fill_color[0], path.fill_color[1], path.fill_color[2]))
+                pix_ref.Recolor(path.fill_color)
+                pix_ref.Copy()
+                np = path.nPoints()
+
+                p_0 = path.getPoint(0)
+                orient_frame2tool = robodk.invH(item_frame.Pose()) * robot.SolveFK(home_trans) * tool_frame.Pose()
+                orient_frame2tool[0:3, 3] = robodk.Mat([0, 0, 0])
+                target0 = robodk.transl(p_0.x, p_0.y, 0) * orient_frame2tool
+                target0_app = target0 * robodk.transl(0, 0, -self._APPROACH)
+                robot.MoveL(target0_app)
+
                 self.RDK.RunMessage('Drawing %s' % path.idname)
                 self.RDK.RunProgram('SetColorRGB(%.3f,%.3f,%.3f)' % (path.fill_color[0], path.fill_color[1], path.fill_color[2]))
                 for i in range(np):
@@ -83,7 +177,7 @@ class Engrave:
             robot.setPoseTool('tool')
 
 
-    # ONLY needed if we used Rotary engraving
+    # Only needed if we do rotary engraving
 
     # if TCP_KEEP_TANGENCY:
     # moving the tool along the path (axis 6 may reach its limits)
