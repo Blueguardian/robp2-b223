4deadf692efab96e10faede7496ff0f612b653a9
Blueguardian <tssa20@student.aau.dk>
2021-05-19 00:25:47 +0200

    Added averages of times and a set function

--- statistics.py
+++ statistics.py
@@ -14,7 +14,8 @@ class Statistics:
     TYPES = ['black_none', 'black_none_engraved', 'black_edge', 'black_edge_engraved', 'black_curved',
              'black_curved_engraved', 'white_none', 'white_none_engraved', 'white_edge', 'white_edge_engraved',
              'white_curved', 'white_curved_engraved', 'blue_none', 'blue_none_engraved', 'blue_edge',
-             'blue_edge_engraved', 'blue_curved', 'blue_curved_engraved']
+             'blue_edge_engraved', 'blue_curved', 'blue_curved_engraved', 'average_production_time',
+             'average_engraving_time', 'average_total_time']
     RDK = Robolink()
 
     def __init__(self):
@@ -34,8 +35,9 @@ class Statistics:
         statistics_ = str(statistics_)
         statistics_ = str(statistics_).replace(', ', '\n').replace('{', '').replace('}', '').replace('\'', '').replace(
             'curved', 'curved covers').replace('_', ' ').replace('none', 'flat covers').replace(
-            'edge', 'curved edges covers').replace('covers:', 'covers produced:')
-        statistics_ = str('Current production statistics:\n' + statistics_)
+            'edge', 'curved edges covers').replace('covers:', 'covers produced:').replace(
+            'average production', '\n\naverage production').replace('time', 'time*')
+        statistics_ = str('Current production statistics:\n' + statistics_ + '\n*All times in seconds')
         self.RDK.ShowMessage(statistics_)
 
     def __create_statistics(self):
@@ -80,4 +82,14 @@ class Statistics:
         """
         statistics_ = self.__get_statistics()
         statistics_[stat_] += count
+        self.__update_statistics(statistics_)
+
+    def set(self, stat_: str, amount: float):
+        """
+        Sets the given statistic to the given amount
+        :param stat_: the type that has to be changed
+        :param amount: The amount to set it to
+        """
+        statistics_ = self.__get_statistics()
+        statistics_[stat_] = amount
         self.__update_statistics(statistics_)
